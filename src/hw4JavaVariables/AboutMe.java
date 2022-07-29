1ec164e7bd4b923f50b3c31ec9ab14879a5be0a4
mafruha maula <maulamafruha@gamil.com>
2022-07-29 16:13:35 -0400

    update

--- src/hw4JavaVariables/AboutMe.java
+++ src/hw4JavaVariables/AboutMe.java
@@ -2,7 +2,7 @@ package hw4JavaVariables;
 
 public class AboutMe {
 	
-	public String name; // variable is declared
+	public String name; //variable is declared
 	public byte age;
 	public short myYearlyHouseRent;
 	public int myYearlySalary;
@@ -12,10 +12,10 @@ public class AboutMe {
 	public char sex;
 	public boolean usCitizen;
 	
-	public AboutMe() { // Constructor declared
+	public AboutMe() { //Constructor declared
 		System.out.println("This is all about us:");
 	}
-	public void aboutMe() { // method is implemented
+	public void aboutMe() { //method is implemented
 	    System.out.println("My Name is: " + name + "\nMy Age: " + age + "\nYearlyHouseRent: " + myYearlyHouseRent
 					+ "\nmyYearlySalary:" + myYearlySalary + "\nMy Bank Balance is: " + myBankBalance + "\nMy Height is: "
 					+ myHeight + "\nMy Grade is: " + myGrade + "\nSex: " + sex + "\nAm I US Citizen? Ans: " + usCitizen);
--- src/hw4JavaVariables/AboutMeTest.java
+++ src/hw4JavaVariables/AboutMeTest.java
@@ -2,10 +2,10 @@ package hw4JavaVariables;
 
 public class AboutMeTest {
 	public static void main(String[] args) {
-		// class is instantiated
-		AboutMe me1 = new AboutMe(); // Constructor is initialized
+		//class is instantiated
+		AboutMe me1 = new AboutMe(); //Constructor is initialized
 		
-		me1.name = "Mafruha Maula"; // variable is initialized
+		me1.name = "Mafruha Maula"; //variable is initialized
 		me1.age = 100;
 		me1.myYearlyHouseRent = 4500;
 		me1.myYearlySalary = 300000;
@@ -14,12 +14,12 @@ public class AboutMeTest {
 		me1.myGrade = 4.0000;
 		me1.sex = 'F';
 		me1.usCitizen = true;
-		me1.aboutMe(); // method initialized
+		me1.aboutMe(); //method initialized
 		
 		System.out.println("\n####################\n");
 
-		AboutMe myfrd = new AboutMe(); // Constructor is initialized
-		myfrd.name = "Alex"; // variable is initialized
+		AboutMe myfrd = new AboutMe(); //Constructor is initialized
+		myfrd.name = "Alex"; //variable is initialized
 		myfrd.age = 90;
 		myfrd.myYearlyHouseRent = 6500;
 		myfrd.myYearlySalary = 200000;
@@ -28,6 +28,6 @@ public class AboutMeTest {
 		myfrd.myGrade = 3.5000;
 		myfrd.sex = 'M';
 		myfrd.usCitizen = true;
-		myfrd.aboutMe(); // method initialized
+		myfrd.aboutMe(); //method initialized
 	}
 }
